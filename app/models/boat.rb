a46fb570fcccffff17b91003bba144fa0d558975
Andy Feidje Joseph <ingandy75@gmail.com>
2019-11-01 03:24:57 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -27,6 +27,10 @@ def self.sailboats
   includes(:classifications).where(classifications: { name: 'Sailboat'})
 end
 
+def self.with_three_classifications
+  joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+end
+
 def self.non_sailboats
     where("id NOT IN (?)", self.sailboats.pluck(:id))
 end
